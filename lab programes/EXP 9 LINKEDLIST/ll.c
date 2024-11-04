a3b91fead83c5014465c28ef9ae2a8dfc11ed659
Annliyaviju <vijuannliya@gmail.com>
2024-11-04 11:05:21 +0530

    Update ll.c

--- lab programes/EXP 9 LINKEDLIST/ll.c
+++ lab programes/EXP 9 LINKEDLIST/ll.c
@@ -1,1 +1,100 @@
 
+//Annliya Viju
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertStart(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+void insertEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void insertBetween(struct Node** head, int data, int X) {
+    struct Node* newNode = createNode(data);
+    struct Node* temp = *head;
+    while (temp != NULL && temp->data != X) {
+        temp = temp->next;
+    }
+    if (temp != NULL) {
+        newNode->next = temp->next;
+        temp->next = newNode;
+    } else {
+        printf("Value %d not found in the list. Insertion failed.\n", X);
+        free(newNode);
+    }
+}
+
+void deleteStart(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty. Deletion failed.\n");
+        return;
+    }
+    struct Node* temp = *head;
+    *head = (*head)->next;
+    free(temp);
+}
+
+void deleteEnd(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty. Deletion failed.\n");
+        return;
+    }
+    struct Node* temp = *head;
+    struct Node* prev = NULL;
+    if (temp->next == NULL) {
+        free(temp);
+        *head = NULL;
+        return;
+    }
+    while (temp->next != NULL) {
+        prev = temp;
+        temp = temp->next;
+    }
+    free(temp);
+    prev->next = NULL;
+}
+
+void deleteAfter(struct Node** head, int X) {
+    struct Node* temp = *head;
+    while (temp != NULL && temp->data != X) {
+        temp = temp->next;
+    }
+    if (temp != NULL && temp->next != NULL) {
+        struct Node* toDelete = temp->next;
+        temp->next = toDelete->next;
+        free(toDelete);
+    } else {
+        printf("Value %d not found or does not have a next node. Deletion failed.\n", X);
+    }
+}
+
+void displayList(struct Node* node) {
+    if (node == NULL) {
+        printf("List is empty.\n");
+        return;
